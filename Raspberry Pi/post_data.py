56949168e0972f970ca183d15e14c3c0dbf24286
pduebel <pnduebel@gmail.com>
2021-05-16 17:45:48 +0100

    simplify post_data

--- Raspberry Pi/post_data.py
+++ Raspberry Pi/post_data.py
@@ -3,12 +3,9 @@ import sqlite3
 import requests
 import pandas as pd
 
-conn = sqlite3.connect('processed_test.db')
+conn = sqlite3.connect('energy.db')
 
 df = pd.read_sql('SELECT * FROM energy', con=conn)
-df = df.drop_duplicates(subset='timestamp', keep='last')
-df['timestamp'] = pd.to_datetime(df['timestamp'], format='%Y%m%d%H%M')
-df['timestamp'] = df['timestamp'].astype(str)
 json = df.to_json()
 
 r = requests.post('https://carbon-meter.herokuapp.com/data-upload', json=json)
