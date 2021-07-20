a2ea51a591c0b8f080529e641dc6c4152bb3c23c
pduebel <pnduebel@gmail.com>
2021-07-20 08:42:49 +0100

    catch exception when scanner stops on restart

--- Raspberry Pi/receive_puck_ble.py
+++ Raspberry Pi/receive_puck_ble.py
@@ -80,7 +80,10 @@ while True:
     # restart script if no data received after 5 loops
     if tries >= 5:
         print('Restarting')
-        scanner.stop()
+        try:
+            scanner.stop()
+        except:
+            pass
         sys.stdout.flush()
         os.execv(sys.executable, ['python3'] + sys.argv)
 # in case were wanted to finish, we should call 'stop'
