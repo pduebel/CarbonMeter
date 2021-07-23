65601a1c5195c05a0de842c6d1e631680b8c3a91
pduebel <pnduebel@gmail.com>
2021-07-23 08:40:49 +0100

    continue testing carbon intesity - combine api request with selecting min, max timestamps from db

--- Raspberry Pi/tests/test_add_carbon.py
+++ Raspberry Pi/tests/test_add_carbon.py
@@ -1,16 +1,49 @@
 import sqlite3
 import requests
+import math
+import datetime as dt
+
 
 conn = sqlite3.connect('test.db')
 cur = conn.cursor()
 
-no_carbon = '''SELECT *
-               FROM energy
-               WHERE carbon_intensity IS NULL'''
 cur.execute('''SELECT
                  MIN(timestamp),
                  MAX(timestamp)
                FROM energy
                WHERE carbon_intensity IS NULL''')
-for row in cur.fetchall():
-    print(row)
+
+min_date_str, max_date_str = cur.fetchall()[0]
+date_format = '%Y-%m-%d %H:%M:%S'
+
+headers = {'Accept': 'application/json'}
+postcode = 'RH2'
+min_date = dt.datetime.strptime(min_date_str, date_format)
+max_date = dt.datetime.strptime(max_date_str, date_format) + dt.timedelta(minutes=30)
+    
+diff = max_date - min_date
+steps = math.floor(diff / dt.timedelta(days=13))
+dates = [min_date.isoformat(), max_date.isoformat()]
+prev_date = min_date
+for i in range(steps):
+    prev_date = prev_date + dt.timedelta(days=13)
+    dates.insert(-1, prev_date.isoformat())
+
+intensity_data = []
+for i in range(len(dates) - 1):
+    date_from = dates[i]
+    date_to = dates[i + 1]
+    r = requests.get(f'https://api.carbonintensity.org.uk/regional/intensity/{date_from}/{date_to}/postcode/{postcode}',
+                     headers=headers)
+
+    for data_dict in r.json()['data']['data']:
+        intensity_dict = {'timestamp': data_dict['from'],
+                          'carbon intensity': data_dict['intensity']['forecast'],
+                          'intensity index': data_dict['intensity']['index']}
+        intensity_data.append(intensity_dict)
+
+# next loop through timestamps in intensity data and insert intensity values where
+# db timestamp >= intensity timestamp but < intensity timestamp + 30 mins
+for intensity_dict in intensity_data:
+    print(intensity_dict['timestamp'])
+    print(dt.datetime.strptime(intensity_dict['timestamp'], '%Y-%m-%dT%H:%MZ'))
--- Raspberry Pi/tests/test_ci_api.py
+++ Raspberry Pi/tests/test_ci_api.py
@@ -5,26 +5,23 @@ import math
 headers = {'Accept': 'application/json'}
 
 postcode = 'RH2'
-min_date = dt.datetime(2021, 7, 1, 0, 0)
-max_date = dt.datetime(2021, 7, 20, 18, 30)
+min_date = dt.datetime(2021, 5, 1, 0, 0)
+max_date = dt.datetime(2021, 5, 1, 0, 30)
 diff = max_date - min_date
 steps = math.floor(diff / dt.timedelta(days=13))
 dates = [min_date.isoformat(), max_date.isoformat()]
 prev_date = min_date
 for i in range(steps):
-    step_date = prev_date + dt.timedelta(days=13)
-    dates.insert(-1, step_date.isoformat())
+    prev_date = prev_date + dt.timedelta(days=13)
+    dates.insert(-1, prev_date.isoformat())
 
 intensity_data = []
 for i in range(len(dates) - 1):
     date_from = dates[i]
     date_to = dates[i + 1]
 
-    print(date_from)
-
     r = requests.get(f'https://api.carbonintensity.org.uk/regional/intensity/{date_from}/{date_to}/postcode/{postcode}',
                      headers=headers)
-    print(r.content)
 
     for data_dict in r.json()['data']['data']:
         intensity_dict = {'timestamp': data_dict['from'],
@@ -32,4 +29,7 @@ for i in range(len(dates) - 1):
                           'intensity index': data_dict['intensity']['index']}
         intensity_data.append(intensity_dict)
 
-print(intensity_data)
+for intensity_dict in intensity_data:
+    print(intensity_dict['timestamp'])
+    print(dt.datetime.strptime(intensity_dict['timestamp'], '%Y-%m-%dT%H:%MZ') + dt.timedelta(minutes=30))
+    
