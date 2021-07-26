f94df7513e11ea9cce799768ff207e8742773add
pduebel <pnduebel@gmail.com>
2021-07-26 08:55:20 +0100

    test carbon intensity insert statement

--- Raspberry Pi/tests/test_add_carbon.py
+++ Raspberry Pi/tests/test_add_carbon.py
@@ -20,6 +20,8 @@ headers = {'Accept': 'application/json'}
 postcode = 'RH2'
 min_date = dt.datetime.strptime(min_date_str, date_format)
 max_date = dt.datetime.strptime(max_date_str, date_format) + dt.timedelta(minutes=30)
+
+print(max_date, min_date)
     
 diff = max_date - min_date
 steps = math.floor(diff / dt.timedelta(days=13))
@@ -42,8 +44,11 @@ for i in range(len(dates) - 1):
                           'intensity index': data_dict['intensity']['index']}
         intensity_data.append(intensity_dict)
 
-# next loop through timestamps in intensity data and insert intensity values where
-# db timestamp >= intensity timestamp but < intensity timestamp + 30 mins
+# need to use update rather than insert into
 for intensity_dict in intensity_data:
-    print(intensity_dict['timestamp'])
-    print(dt.datetime.strptime(intensity_dict['timestamp'], '%Y-%m-%dT%H:%MZ'))
+    timestamp = dt.datetime.strptime(intensity_dict['timestamp'], '%Y-%m-%dT%H:%MZ')
+    from_timestamp = str(timestamp)
+    to_timestamp = str(timestamp + dt.timedelta(minutes=30))
+    print(from_timestamp, to_timestamp)
+    for row in cur.execute("INSERT INTO energy (carbon_intensity) VALUES (12) WHERE timestamp > ?", (from_timestamp,)):
+        print(row)
