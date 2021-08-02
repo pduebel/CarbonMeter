bc083ee1eb43c8ba413de7bad2346f9fb17635d3
pduebel <pnduebel@gmail.com>
2021-08-02 08:29:19 +0100

    add error handling to db class & minor sql styling

--- Raspberry Pi/db.py
+++ Raspberry Pi/db.py
@@ -47,21 +47,25 @@ class DB:
         -------
         None
         '''
+        try:
+            conn = sqlite3.connect(self.db_name)
+            c = conn.cursor()
+            c.execute('''CREATE TABLE IF NOT EXISTS energy (
+                           timestamp TEXT PRIMARY KEY,
+                           battery INTEGER,
+                           total_kWh FLOAT,
+                           kWh FLOAT,
+                           kW FLOAT,
+                           carbon_intensity INTEGER,
+                           intensity_index TEXT,
+                           carbon INTEGER
+                          );''')
+            conn.commit()
+            conn.close()
         
-        conn = sqlite3.connect(self.db_name)
-        c = conn.cursor()
-        c.execute('''CREATE TABLE IF NOT EXISTS energy (
-                         timestamp TEXT PRIMARY KEY,
-                         battery INTEGER,
-                         total_kWh FLOAT,
-                         kWh FLOAT,
-                         kW FLOAT,
-                         carbon_intensity INTEGER,
-                         intensity_index TEXT,
-                         carbon INTEGER
-                      );''')
-        conn.commit()
-        conn.close()
+        except Exception as e:
+            print(e)
+            print('Error creating table')
     
     def insert(self, data):
         '''
@@ -85,15 +89,36 @@ class DB:
         None
         '''
         
-        conn = sqlite3.connect(self.db_name)
-        c = conn.cursor()
-        c.execute('''REPLACE INTO energy (timestamp, battery, total_kWh, kWh, kW)
-                     VALUES (?, ?, ?, (? - (SELECT total_kWh
-                                            FROM energy
-                                            WHERE timestamp = (SELECT MAX(timestamp)
-                                                               FROM energy))), ?)''', data)
-        conn.commit()
-        conn.close()
+        try:
+            conn = sqlite3.connect(self.db_name)
+            c = conn.cursor()
+            c.execute('''REPLACE INTO energy (
+                           timestamp,
+                           battery,
+                           total_kWh,
+                           kWh,
+                           kW
+                         )
+                         VALUES (
+                           ?,
+                           ?,
+                           ?,
+                           (? - (
+                               SELECT total_kWh
+                               FROM energy
+                               WHERE timestamp = (
+                                   SELECT MAX(timestamp)
+                                   FROM energy
+                               )
+                           )),
+                           ?)''',
+                      data)
+            conn.commit()
+            conn.close()
+            
+        except Exception as e:
+            print(e)
+            print('Error inserting data into db')
         
     def post_data(self, post_url):
         '''
@@ -110,88 +135,105 @@ class DB:
         None
         '''
         
-        conn = sqlite3.connect(self.db_name)
-        df = pd.read_sql('SELECT * FROM energy', con=conn)
-        json = df.to_json()
-        r = requests.post(post_url, json=json)
-        print(r.content)
+        try:
+            conn = sqlite3.connect(self.db_name)
+            df = pd.read_sql('SELECT * FROM energy', con=conn)
+            json = df.to_json()
+            r = requests.post(post_url, json=json)
+            r.raise_for_status()
+            
+        except Exception as e:
+            print(e)
+            print('Error uploading db')
         
-    def get_carbon_intensity(self):
+    def get_carbon_intensity(self, postcode):
         '''
         Finds min and max times of records that are missing carbon intensity
-        data and gets carbon intensity data (in gCO2/kWh) across this range by
-        querying the National Grid API. Then updates records with this data where
-        missing. Also multiplies kWh by carbon intensity to give gCO2 produced.
+        data and gets regional carbon intensity data (in gCO2/kWh) across this
+        range by querying the National Grid API. Then updates records with this
+        data where missing. Also multiplies kWh by carbon intensity to give gCO2
+        produced.
+        
+        Carbon intensity data is for the UK only, with regional data currently in
+        beta so only able to use forecasted values.
         
         Parameters
         ----------
-        None
+        postcode: string
+            Outward part (the first part) of user postcode (UK only) for providing
+            regional carbon intensity data.
         
         Returns
         -------
         None
         '''
         
-        conn = sqlite3.connect(self.db_name)
-        cur = conn.cursor()
-        # find min and max datetimes that are missing data
-        cur.execute('''SELECT
-                         MIN(timestamp),
-                         MAX(timestamp)
-                       FROM energy
-                       WHERE carbon_intensity IS NULL''')
+        try:
+            conn = sqlite3.connect(self.db_name)
+            cur = conn.cursor()
+            # find min and max datetimes that are missing data
+            cur.execute('''SELECT
+                             MIN(timestamp),
+                             MAX(timestamp)
+                           FROM energy
+                           WHERE carbon_intensity IS NULL''')
 
-        min_date_str, max_date_str = cur.fetchall()[0]
+            min_date_str, max_date_str = cur.fetchall()[0]
 
-        if min_date_str and max_date_str:
-            headers = {'Accept': 'application/json'}
-            postcode = 'RH2'
-            date_format = '%Y-%m-%d %H:%M:%S'
-            min_date = dt.datetime.strptime(min_date_str, date_format)
-            max_date = dt.datetime.strptime(max_date_str, date_format) + dt.timedelta(minutes=30)
+            if min_date_str and max_date_str:
+                headers = {'Accept': 'application/json'}
+                postcode = postcode
+                date_format = '%Y-%m-%d %H:%M:%S'
+                min_date = dt.datetime.strptime(min_date_str, date_format)
+                max_date = dt.datetime.strptime(max_date_str, date_format) + dt.timedelta(minutes=30)
             
-            # API can only work in date ranges <14 days - so break the date
-            # range into 13 day chunks
-            diff = max_date - min_date
-            steps = math.floor(diff / dt.timedelta(days=13))
-            dates = [min_date.isoformat(), max_date.isoformat()]
-            prev_date = min_date
-            for i in range(steps):
-                prev_date = prev_date + dt.timedelta(days=13)
-                dates.insert(-1, prev_date.isoformat())
+                # API can only work in date ranges <14 days - so break the date
+                # range into 13 day chunks
+                diff = max_date - min_date
+                steps = math.floor(diff / dt.timedelta(days=13))
+                dates = [min_date.isoformat(), max_date.isoformat()]
+                prev_date = min_date
+                for i in range(steps):
+                    prev_date = prev_date + dt.timedelta(days=13)
+                    dates.insert(-1, prev_date.isoformat())
             
-            # loop through 13 day chunks to call API
-            intensity_data = []
-            for i in range(len(dates) - 1):
-                date_from = dates[i]
-                date_to = dates[i + 1]
-                r = requests.get(f'https://api.carbonintensity.org.uk/regional/intensity/{date_from}/{date_to}/postcode/{postcode}',
-                                 headers=headers)
-            for data_dict in r.json()['data']['data']:
-                intensity_dict = {'timestamp': data_dict['from'],
-                                  'carbon intensity': data_dict['intensity']['forecast'],
-                                  'intensity index': data_dict['intensity']['index']}
-                intensity_data.append(intensity_dict)
+                # loop through 13 day chunks to call API
+                intensity_data = []
+                for i in range(len(dates) - 1):
+                    date_from = dates[i]
+                    date_to = dates[i + 1]
+                    r = requests.get(f'https://api.carbonintensity.org.uk/regional/intensity/{date_from}/{date_to}/postcode/{postcode}',
+                                     headers=headers)
+                # extract relevant data from response
+                for data_dict in r.json()['data']['data']:
+                    intensity_dict = {'timestamp': data_dict['from'],
+                                      'carbon intensity': data_dict['intensity']['forecast'],
+                                      'intensity index': data_dict['intensity']['index']}
+                    intensity_data.append(intensity_dict)
 
-            # add data to db
-            for intensity_dict in intensity_data:
-                timestamp = dt.datetime.strptime(intensity_dict['timestamp'],
-                                                 '%Y-%m-%dT%H:%MZ')
-                from_timestamp = str(timestamp)
-                to_timestamp = str(timestamp + dt.timedelta(minutes=30))
-                cur.execute('''UPDATE energy
-                               SET
-                                 carbon_intensity = ?,
-                                 intensity_index = ?,
-                                 carbon = (? * kWh)
-                               WHERE timestamp >= ?
-                                 AND timestamp < ?
-                                 AND carbon_intensity IS NULL''',
-                            (intensity_dict['carbon intensity'],
-                             intensity_dict['intensity index'],
-                             intensity_dict['carbon intensity'],
-                             from_timestamp,
-                             to_timestamp))
+                # add data to db
+                for intensity_dict in intensity_data:
+                    timestamp = dt.datetime.strptime(intensity_dict['timestamp'],
+                                                     '%Y-%m-%dT%H:%MZ')
+                    from_timestamp = str(timestamp)
+                    to_timestamp = str(timestamp + dt.timedelta(minutes=30))
+                    cur.execute('''UPDATE energy
+                                   SET
+                                     carbon_intensity = ?,
+                                     intensity_index = ?,
+                                     carbon = (? * kWh)
+                                   WHERE timestamp >= ?
+                                     AND timestamp < ?
+                                     AND carbon_intensity IS NULL''',
+                                (intensity_dict['carbon intensity'],
+                                 intensity_dict['intensity index'],
+                                 intensity_dict['carbon intensity'],
+                                 from_timestamp,
+                                 to_timestamp))
 
-        conn.commit()
-        conn.close()
+            conn.commit()
+            conn.close()
+            
+        except Exception as e:
+            print(e)
+            print('Error getting carbon intensity data')
