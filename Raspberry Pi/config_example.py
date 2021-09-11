5d735d66d3a6e11cd3ed8ce831dcb289f24bc21d
pduebel <pnduebel@gmail.com>
2021-09-11 12:04:44 +0100

    add missing commas to config example

--- Raspberry Pi/config_example.py
+++ Raspberry Pi/config_example.py
@@ -2,12 +2,10 @@ config = {
     'DEVICE': 'h7:04:7a:c2:27:10', # ip address of puck
     'DB_PATH': '/home/pi/energy.db', # filepath to database
     'IMP/KWH': 1000, # can find this next to led on meter, usually 800 or 1000
-    'POSTCODE': 'RH13' # outward part of postcode (first part, UK only)
+    'POSTCODE': 'RH13', # outward part of postcode (first part, UK only)
     'WEB_APP': True, # set to True or False if using web app or not
-    #--- optional ---
-    # following variables for use with web app
     'POST_URL_KW': 'https://pimeter.herokuapp.com/kW-upload', # (optional) url to post kW value to
     'POST_URL_DB': 'https://pimeter.herokuapp.com/data-upload', # (optional) url to post db to
-    'USERNAME': 'admin' #(optional) username for flask web app
+    'USERNAME': 'admin', #(optional) username for flask web app
     'PASSWORD': 'password123' #(optional) password for flask web app
 }
